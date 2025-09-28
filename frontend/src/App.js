17801f775ac85e076ee1bee0792914384f68e4c5
Vijay-Nalla <23eg105m39@anurag.edu.in>
2025-09-29 00:23:29 +0530

    feat: Add main React App component with basic structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Student Dropout Risk Dashboard</h1>
+      {/* Dashboard components will be added here */}
+    </div>
+  );
+}
+
+export default App;
